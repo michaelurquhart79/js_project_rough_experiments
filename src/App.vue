70da36afeac7b177cae3963f16f6882d5a89749c
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-07 10:44:12 +0100

    commit before sharing repo with team.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="">
+<div class="">
+  <chart-view :chart_data="chart_data"></chart-view>
+
+  <p v-for="item in stock_data">{{item.symbol}}</p>
+  <button v-on:click="stockgraph">Click me to do stuff</button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChartView from "@/components/Chart";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'chart-view': ChartView
+  },
+  data() {
+    return {
+      stock_tickers: ['CMCSA', 'CMCSA', 'CMCSA', 'CMCSA', 'CMCSA', 'CMCSA',],
+      stock_data: [],
+      // api_key: "4CDV35QACU2KPB3U"
+      chart_data: [10, 0, 8, 2, 6, 4, 5, 5],
+      // chart_data: [],
+      test_variable: []
+    }
+  },
+  mounted() {
+
+      fetch('https://financialmodelingprep.com/api/v3/historical-price-full/AAPL,GOOG,CMCSA?serietype=line')
+      .then(res => res.json())
+      // .then(data => this.chart_data=data["historicalStockList"][0]["historical"])
+      .then(data => this.chart_data=[3,5,7,3,6])
+
+
+    // fetch
+  },
+  methods: {
+    stockgraph: function(){
+      // let i = this.stock_data
+      // .historical[-1].close;
+       // const dummy= this.stock_data["historicalStockList"][0]["historical"];
+       // this.test_variable = dummy[dummy.length -1].close
+       this.chart_data = this.stock_data["historicalStockList"][0]["historical"];
+
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+ <highcharts :options="chartOptions2"></highcharts>
+</template>
+
+<script>
+import {Chart} from 'highcharts-vue'
+export default {
+  name: 'chart-view',
+  props: ['chart_data'],
+  components: {
+    highcharts: Chart
+  },
+  data() {
+    return {
+      // title: '',
+      // points: [10, 0, 8, 2, 6, 4, 5, 5],
+      // chartType: 'Spline',
+      // seriesColor: '#6fcd98',
+      // colorInputIsSupported: null,
+      chartOptions2: {
+        chart: {
+          type: 'bar'
+        },
+        title: {
+          text: 'Mikes chart'
+        },
+        series: [{
+          data: this.chart_data,
+          color: 'red'
+        }]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
